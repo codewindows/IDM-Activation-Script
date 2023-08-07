7eebeca114e45583f1143093c3eeddcf8c26d6ff
codewindows <139599431+codewindows@users.noreply.github.com>
2023-08-07 23:07:53 +0800

    Update IAS_0.8.cmd

--- IAS_0.8.cmd
+++ IAS_0.8.cmd
@@ -183,9 +183,8 @@ set _col=%_Yellow%
 
 echo:          _____________________________________________   
 echo: 
-echo:             "IDM Activation & Trail Reset Script"
-call :_color2 %_White% "        " %_Green% "  Create By Piash - Edited By CodeWindows"
-echo: 
+echo:             IDM Activation & Trail Reset Script
+call :_color2 %_White% "        " %_Green% " Create By Piash - Edited By CodeWindows"
 echo:          _____________________________________________   
 echo:                                                          
 echo:          [1] Activate IDM                                
